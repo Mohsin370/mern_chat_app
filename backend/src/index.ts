4b89f7b1c619003c25f3045097b40271877ff0de
Mohsin370 <mohsinijaz13@gmail.com>
2024-04-14 20:13:11 +0930

    backend cors error fix -temp

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,15 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to chat application!");
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.removeHeader('x-powered-by');
+  res.setHeader('Access-Control-Allow-Methods','POST');
+  res.setHeader('Access-Control-Allow-Headers','Content-Type');
+  next();
+});
+
+
 
 app.use("/api", routes);
 
