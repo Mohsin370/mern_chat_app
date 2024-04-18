960aed509a9cd1c8e7eb9a4c973f5011a67fc41a
Mohsin370 <mohsinijaz13@gmail.com>
2024-04-18 10:39:26 +0930

    useEffect to avoid memory leaks and best practice

--- frontend/src/components/notification.tsx
+++ frontend/src/components/notification.tsx
@@ -1,17 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { NotificationContext } from "../context/notification/notificationContext";
 
 export default function Notification() {
   const { notification, setNotification } = useContext(NotificationContext);
-  if (notification.show) {
-    setTimeout(() => {
-      setNotification({ show: false, message: "", type: "" });
-    }, 3000);
-  }
+
+  useEffect(() => {
+    if (notification.show) {
+      setTimeout(() => {
+        setNotification({ show: false, message: "", type: "" });
+      }, 3000);
+    }
+  }, [notification, setNotification]);
+
   return (
     notification.show && (
       <div className="absolute top-5 right-5">
-        <div className=" bg-secondary p-5 rounded-sm text-white">{notification.message}</div>
+        <div className=" bg-secondary p-5 rounded-sm text-white">
+          {notification.message}
+        </div>
       </div>
     )
   );
