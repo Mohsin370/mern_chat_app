c505325f05cba2d217cface539bce91b58bebfff
Khawaja Mohsin <mohsinijaz13@gmail.com>
2024-05-01 16:33:20 +0930

    chatContext provider added in root file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,12 +5,15 @@ import Routes from "./Routes";
 import { NotificationProvider } from "./context/notification/notificationContext";
 import Notification from "./components/notification";
 import { AuthContextProvider } from "./context/auth/authContext";
+import { ChatContextProvider } from "./context/chat/chatContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NotificationProvider>
       <AuthContextProvider>
-        <Routes />
+        <ChatContextProvider>
+          <Routes />
+        </ChatContextProvider>
       </AuthContextProvider>
       <Notification />
     </NotificationProvider>
