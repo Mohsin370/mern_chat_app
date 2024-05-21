8af23d2d845b7739d163acb7c4947adadcd7edc5
Khawaja Mohsin <mohsinijaz13@gmail.com>
2024-05-21 11:08:55 +0930

    text ellipsis

--- frontend/src/components/chatTab.tsx
+++ frontend/src/components/chatTab.tsx
@@ -15,10 +15,10 @@ const ChatTab = (props: tabProps) => {
     <div className="flex rounded-md pb-5">
       <ProfileImg image={props.image} name={props.name} />
 
-      <div className="flex w-full justify-between">
+      <div className="flex w-full justify-between overflow-hidden">
         <div className="flex flex-col">
           <span className="font-bold tracking-wider">{props.name}</span>
-          <span className="text-xs">{props.typing ?"Typing...": props.lastMsg}</span> 
+          <span className="text-xs max-w-[250px] whitespace-nowrap overflow-hidden text-ellipsis">{props.typing ?"Typing...": props.lastMsg}</span> 
         </div>
         <span className="text-xs text-gray-400">{props.time}</span>
       </div>
